0c505d6d3005d4cf6a2c433d3eb977bf7f767717
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2026-08-05 15:08:00 +0530

    Problem 6 solved

--- problem_set_1.py
+++ problem_set_1.py
@@ -183,3 +183,22 @@ orders = [("Masala Chai", 3, 20), ("Samosa", 2,
 15), ("Green Tea", 1, 30)]
 
 smartBillingEngine(orders)
+
+print("---------------------------------------------------------")
+#problem 6
+
+
+
+
+groupData1 = [("Brazil", 3, 0, 0), ("Japan", 1, 2,
+0), ("Spain", 2, 0, 1), ("Ghana", 0, 1,
+2)]
+
+
+
+pts = list(filter((lambda x: (x[1] * 3 + x[2]) >= 6 and x[3] <= 1), groupData1))
+
+print("Advancing to knockouts: ")
+for team, wins, draws, losses in pts:
+    print(team, " -", wins * 3, "pts")
+ 
