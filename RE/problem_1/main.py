9c9cf1e200398e3a9508d91d7c1533f97d460a74
IshaHarmalkar <167905999+IshaHarmalkar@users.noreply.github.com>
2026-08-15 20:15:31 +0530

    Problem 1 solved

--- RE/problem_1/main.py
+++ RE/problem_1/main.py
@@ -3,16 +3,17 @@ import re
 
 text = "John Doe: 28, Alice Smith: 34, Bob: 19, Charlie Brown: 45"
 
-#Name : Age
-#name  -> alpha + spaces
-#age = number
+nameExp = r"[a-zA-z]+[\s]?[a-zA-Z]*:"  
 
-#exp = '^[A-Za-z]:\d+$'
-#res = re.findall(r"[A-Za-z+:\d+", text)
+ageExp = r"[\s][1-9]+[0-9]*"
 
-#names = re.findall(r"[A-Za-z\s+?]\s*", text)
+nameMatches = re.findall(nameExp, text)
 
-names = re.findall(r"([A-Za-z\s]+?):\s*\d+", text)
-names = [name.strip() for name in names]
+nameMatches = [s.replace(":", "") for s in nameMatches]
+ageMatches = re.findall(ageExp, text)
 
-print(names)
+#print("Name Matches: ", nameMatches)
+#print("Age Matches: ", ageMatches)
+
+for i in range(len(nameMatches)):
+    print(f"{nameMatches[i]} - {ageMatches[i]}")
